331b9006550cb5acae3342677c8bda29ad27e2ea
wangchengkun <wangck@iccgame.com>
2026-01-16 21:27:25 +0800

    修改部署脚本

--- deploy-react.sh
+++
@@ -1,5 +0,0 @@
-git pull
-
-rm -rf /var/www/interview-app
-
-cp -r /root/apps/InterviewQuestions/nest-react/build /var/www/interview-app
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+echo "开始拉取代码"
+git pull
+echo "拉取代码完成"
+
+cd /root/apps/InterviewQuestions/nest
+echo "开始打包nest"
+pnpm build
+echo "nest打包结束"
+
+echo "pm2重启nest-backend"
+pm2 restart nest-backend
+echo "重启nest-backend完成"
+
+rm -rf /var/www/interview-app
+
+echo "开始拷贝文件到服务器目录"
+cp -r /root/apps/InterviewQuestions/nest-react/build /var/www/interview-app
+echo "拷贝文件到服务器目录完成"
