2b8506e9841a023b8c6a72fa9c6b07b4e32edfa5
wangchengkun <wangck@iccgame.com>
2026-01-16 21:12:30 +0800

    fix: 接口

--- nest-react/src/services/imApi.ts
+++ nest-react/src/services/imApi.ts
@@ -6,7 +6,7 @@
 
 import axios from "axios";
 
-const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}api/im`;
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/im`;
 
 // 创建 axios 实例
 const api = axios.create({
